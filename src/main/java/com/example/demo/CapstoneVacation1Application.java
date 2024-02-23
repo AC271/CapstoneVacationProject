4baae75b94c3695e414ca27ff9c9683b79afed95
AC271 <ac271@students.uwf.edu>
2024-02-22 19:33:42 -0600

    initial commit or hello world

---
+++ src/main/java/com/example/demo/CapstoneVacation1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneVacation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneVacation1Application.class, args);
+	}
+
+}
